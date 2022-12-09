0253a1f758def329680fe764b8c5836489f3d21f
levi <tdnam.cmc@gmail.com>
2022-12-09 14:00:16 +0700

    Initial Commit

---
+++ ios_list_data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_list_data
+//
+//  Created by Nam on 09/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
