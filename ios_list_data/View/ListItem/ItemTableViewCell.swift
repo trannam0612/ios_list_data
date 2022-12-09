a91ff9239e830851647781625c77ed9814008fbb
levi <tdnam.cmc@gmail.com>
2022-12-09 16:08:08 +0700

    first commit

---
+++ ios_list_data/View/ListItem/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  ios_list_data
+//
+//  Created by Nam on 09/12/2022.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
